6cd66672b6fd2051860a6fafd63ca04b545e0ede
Hossam <hossameldinali96@gmail.com>
2020-06-24 02:42:53 +0200

    Update Product Controller

---
+++ GroceryStore.Web/ApiModels/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Web.ApiModels
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<string> ImgUrl { get; set; }
+    }
+}
